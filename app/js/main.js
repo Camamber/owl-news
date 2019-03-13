d1d7d5b4d14c3955128f5222998f47c099bbcd3d
Camamber <egorka.k2009@gmail.com>
2019-03-13 09:40:59 +0200

    colored buttons

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,13 @@ btn_grid.addEventListener('click', function() {
 	if (!btn_grid.classList.contains('active')) {
 		btn_grid.classList.add('active');
 		btn_flow.classList.remove('active');
+
+		btn_flow.getElementById('grey').style.display = 'block';
+		btn_flow.getElementById('red').style.display = 'none';		
+		btn_grid.getElementById('grey').style.display = 'none';
+		btn_grid.getElementById('red').style.display = 'block';		
+
+
 		feed.classList.remove('flow-feed');
 		feed.classList.add('grid-feed');
 	}
@@ -16,6 +23,12 @@ btn_flow.addEventListener('click', function() {
 	if (!btn_flow.classList.contains('active')) {
 		btn_flow.classList.add('active');
 		btn_grid.classList.remove('active');
+
+		btn_grid.getElementById('grey').style.display = 'block';
+		btn_grid.getElementById('red').style.display = 'none';		
+		btn_flow.getElementById('grey').style.display = 'none';
+		btn_flow.getElementById('red').style.display = 'block';		
+
 		feed.classList.remove('grid-feed');
 		feed.classList.add('flow-feed');
 	}
