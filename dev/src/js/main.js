c6133b5a61805b247b730ace3a206920d63f873b
EgerbSher <egorka.k2009@ukr.net>
2019-03-18 22:50:04 +0200

    second page

--- dev/src/js/main.js
+++ dev/src/js/main.js
@@ -4,7 +4,8 @@ var btn_grid = document.getElementById('grid-view'),
 	btn_flow = document.getElementById('flow-view'),
 	feed = document.getElementById('feed');
 
-btn_grid.addEventListener('click', function() {
+
+function grid() {
 	if (!btn_grid.classList.contains('active')) {
 		btn_grid.classList.add('active');
 		btn_flow.classList.remove('active');
@@ -18,9 +19,11 @@ btn_grid.addEventListener('click', function() {
 		feed.classList.remove('flow-feed');
 		feed.classList.add('grid-feed');
 	}
-});
+}
 
-btn_flow.addEventListener('click', function() {
+btn_grid.addEventListener('click', grid);
+
+function flow() {
 	if (!btn_flow.classList.contains('active')) {
 		btn_flow.classList.add('active');
 		btn_grid.classList.remove('active');
@@ -33,9 +36,18 @@ btn_flow.addEventListener('click', function() {
 		feed.classList.remove('grid-feed');
 		feed.classList.add('flow-feed');
 	}
+}
+
+btn_flow.addEventListener('click', flow);
+
+$('.btn-more').click(function() {
+	if($('.wrapper').css('grid-template-areas')=='"hl hl hl hl hl hm hm hm hm hm hm hm" "c c c c c c c c c s s s" "fl fl fl fl ft fm fm fm fm fm fs fs"')
+		$('.wrapper').css('grid-template-areas', '"hl hl hl hl hl hm hm hm hm hm hm hm" "s s s c c c c c c c c c" "fl fl fl fl ft fm fm fm fm fm fs fs"');
+	else
+		$('.wrapper').css('grid-template-areas', '"hl hl hl hl hl hm hm hm hm hm hm hm" "c c c c c c c c c s s s" "fl fl fl fl ft fm fm fm fm fm fs fs"');
 });
 
-$(document).ready(function(){
+$(document).ready(function() {
   $('#slider').slick({
   	'arrows': false,
   	'autoplay': true
